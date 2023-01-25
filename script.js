1ec0abcdcab7ea3559983ddc8554497eda58ba8f
GretaFormica <greta.formica@gmail.com>
2023-01-25 15:16:14 +0100

    js + css

---
+++ script.js
@@ -0,0 +1,29 @@
+const list = document.getElementById('list');
+
+for(let i = 1; i <= 100; i++) {
+
+    const item = document.createElement("li");
+
+    item.innerHTML = i;
+    item.classList.add("box");
+
+    list.append(item);
+
+    if(i % 3 == 0) {
+        item.innerHTML = "fizz";
+        item.classList.add("fizz");
+    }
+
+    if(i % 5 == 0) {
+        item.innerHTML = "buzz";
+        item.classList.add("buzz");
+    }
+
+    if((i % 3 == 0) && (i % 5 == 0)) {
+        item.innerHTML = "fizzbuzz";
+        item.classList.add("fizz-buzz");
+    }
+
+
+
+}
